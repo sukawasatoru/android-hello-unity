f96485e6cf480e49d1771ca3123cf56a023021fa
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2021-02-27 19:45:20 +0900

    Remove unused import

--- app/src/main/kotlin/jp/tinyport/hellounity/UnityPlayerActivity.kt
+++ app/src/main/kotlin/jp/tinyport/hellounity/UnityPlayerActivity.kt
@@ -5,8 +5,6 @@ import android.app.Activity
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.MotionEvent
 import android.view.Window
 import com.unity3d.player.UnityPlayer
 
