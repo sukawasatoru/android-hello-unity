4576d8d2cdcce4eb2d402e69b998b0d4f863e5c1
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2021-02-27 19:43:05 +0900

    Launch UnityPlayer via other activity

--- app/src/main/kotlin/jp/tinyport/hellounity/MainActivity.kt
+++ app/src/main/kotlin/jp/tinyport/hellounity/MainActivity.kt
@@ -1,7 +1,19 @@
 package jp.tinyport.hellounity
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import jp.tinyport.hellounity.databinding.MainActivityBinding
 
-class MainActivity: Activity() {
-    // TODO:
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            startActivity(Intent(this, UnityPlayerActivity::class.java))
+        }
+    }
 }
---
+++ app/src/main/kotlin/jp/tinyport/hellounity/UnityPlayerActivity.kt
@@ -0,0 +1,90 @@
+package jp.tinyport.hellounity
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.Window
+import com.unity3d.player.UnityPlayer
+
+class UnityPlayerActivity : Activity() {
+    @Suppress("ProtectedInFinal")
+    @JvmField
+    protected var mUnityPlayer: UnityPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        val cmdLine = intent.getStringExtra("unity")
+        intent.putExtra("unity", cmdLine)
+
+        mUnityPlayer = UnityPlayerLayout(this)
+        setContentView(mUnityPlayer)
+        mUnityPlayer!!.requestFocus()
+    }
+
+    override fun onDestroy() {
+        mUnityPlayer!!.destroy()
+
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mUnityPlayer!!.resume()
+    }
+
+    override fun onPause() {
+        mUnityPlayer!!.pause()
+
+        super.onPause()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        mUnityPlayer!!.newIntent(intent)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        mUnityPlayer!!.configurationChanged(newConfig)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        mUnityPlayer!!.lowMemory()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL) {
+            mUnityPlayer!!.lowMemory()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        mUnityPlayer!!.windowFocusChanged(hasFocus)
+    }
+}
+
+@SuppressLint("ViewConstructor")
+class UnityPlayerLayout(activity: Activity) : UnityPlayer(activity) {
+    override fun onUnityPlayerUnloaded() {
+        currentActivity.moveTaskToBack(true)
+
+        super.onUnityPlayerUnloaded()
+    }
+}
